1f64af3b9c980f6e1339a1f65d7501eefb73e4a2
Adrian Carpenter <adriatic.c@gmail.com>
2019-12-05 06:39:40 -0800

    Add "responsive" 1-3 screen design

--- clock/climateclock.py
+++ clock/climateclock.py
@@ -6,16 +6,19 @@ import sys
 import time
 from datetime import datetime, timezone
 
+import config
 import requests
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
 
-FONT = '9x18B.bdf'
 TIME_COLOR = '#ffd919'
-CO2_COLOR = '#9900e6'
-JSON = 'https://raw.githubusercontent.com/beautifultrouble/climate-clock-widget/master/src/clock.json'
+TIME_ALT_COLOR = '#c8890a'
+BUDGET_COLOR = '#9900e6'
+BUDGET_ALT_COLOR = '#671497'
+
 
 # TODO: Pull these from the network
+JSON = 'https://raw.githubusercontent.com/beautifultrouble/climate-clock-widget/master/src/clock.json'
 SECONDS_PER_YEAR = 365.25 * 24 * 3600
 START_DATE = datetime(2018, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
 START_EMISSIONS_BUDGET = 4.2e11
@@ -44,39 +47,66 @@ def run(options):
     matrix = RGBMatrix(options=options)
     canvas = matrix.CreateFrameCanvas()
 
+    font_file = ['5x8.bdf', '6x13B.bdf', '9x18B.bdf'][options.chain_length - 1]
     font = graphics.Font()
-    font.LoadFont(relpath(FONT))
+    font.LoadFont(relpath(font_file))
 
-    time_color = hex2color(TIME_COLOR)
-    co2_color = hex2color(CO2_COLOR)
+    t_color = hex2color(TIME_COLOR)
+    ta_color = hex2color(TIME_ALT_COLOR)
+    b_color = hex2color(BUDGET_COLOR)
+    ba_color = hex2color(BUDGET_ALT_COLOR)
 
-    while not time.sleep(.25):
+    while not time.sleep(.1):
         time_remaining, emissions_budget = time_and_budget()
 
         years, r = divmod(time_remaining, SECONDS_PER_YEAR)
         days, r = divmod(r, 24 * 3600)
         hours, r = divmod(r, 3600)
         minutes, seconds = divmod(r, 60)
-        time_string = (f"{years:1.0f}{'YR' if years == 1 else 'YRS'} "
-                       f"{days:3.0f}{'DAY' if days == 1 else 'DAYS'} "
-                       f"{hours:02.0f}:{minutes:02.0f}:{seconds:02.0f}")
-
-        budget_string = f"{int(emissions_budget):,} TONS"
 
         canvas.Clear()
-        graphics.DrawText(canvas, font, 1, 14, time_color, time_string)
-        graphics.DrawText(canvas, font, 1, 28, co2_color, budget_string)
+
+        days = days + 200
+        t_strings = [
+            f"{years:1.0f}",
+            f"{'YR' if years == 1 else 'YRS'}",
+            f"{days:3.0f}",
+            f"{'DAY' if days == 1 else 'DAYS'}",
+            f"{hours:02.0f}:{minutes:02.0f}:{seconds:02.0f}",
+        ]
+        b_strings = [
+            f"{int(emissions_budget):,}"[:12],
+            f"{int(emissions_budget):,}"[12:],
+            f"TONS",
+        ]
+
+        # Place text fragments according to number of screens
+        t_colors = [t_color, ta_color, t_color, ta_color, t_color]
+        b_colors = [b_color, b_color, ba_color]
+        if options.chain_length == 3:
+            t_pos = (2, 14), (16, 14), (47, 14), (78, 14), (118, 14)
+            b_pos = (2, 28), (111, 28), (145, 28)
+        elif options.chain_length == 2:
+            t_pos = (2, 14), (10, 14), (31, 14), (51, 14), (78, 14)
+            b_pos = (2, 28), (74, 28), (96, 28)
+        elif options.chain_length == 1:
+            t_pos = (3, 7), (9, 7), (26, 7), (42, 7), (3, 15), (42, 15)
+            t_strings.append(f".{int(seconds%1*1000):03d}")
+            t_colors.append(t_color) 
+            b_pos = (3, 23), (3, 31), (22, 31)
+            b_strings[2] = "TONS CO₂"
+
+        for pos, strings, colors in ((t_pos, t_strings, t_colors), (b_pos, b_strings, b_colors)):
+            for (x, y), s, c in zip(pos, strings, colors):
+                graphics.DrawText(canvas, font, x, y, c, s)
+
         canvas = matrix.SwapOnVSync(canvas)
 
 
 options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
-options.chain_length = 3
-options.gpio_slowdown = (0, 1, 2, 3)[2 - (options.chain_length == 3)]
-# These may be preconfigured in the rgbmatrix library
-options.hardware_mapping = 'adafruit-hat' 
-options.disable_hardware_pulsing = True
+for key, value in vars(config).items():
+    if not key.startswith('__'):
+        setattr(options, key, value)
 
 run(options)
 
