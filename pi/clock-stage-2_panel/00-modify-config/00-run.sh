d2ca9d7b944753c0685499a62354a4bd64ece777
Adrian Carpenter <adriatic.c@gmail.com>
2021-03-22 14:54:36 -0700

    Remove build stages for 1 and 2 panel clocks

--- pi/clock-stage-2_panel/00-modify-config/00-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-    echo "Adapting config for 2 panels..."
-    sed -i '/chain_length/ s/[0-9]\+/2/' /home/${FIRST_USER_NAME}/climate-clock-kit/clock/config.py
-EOF
