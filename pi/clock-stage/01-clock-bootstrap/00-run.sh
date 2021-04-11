2be0789251b1843f7409ed53c362fa8796b90db2
Adrian Carpenter <adriatic.c@gmail.com>
2021-04-11 15:31:44 -0700

    Pi-gen build stage changes for the 2021 build

--- pi/clock-stage/01-clock-bootstrap/00-run.sh
+++ pi/clock-stage/01-clock-bootstrap/00-run.sh
@@ -9,12 +9,14 @@ printf "\n# Faster booting when network isn't present\ntimeout 5\n" >>"${ROOTFS_
 echo "Adding climateclock systemd service..."
 cat >"${ROOTFS_DIR}/lib/systemd/system/climateclock.service" <<EOF
 [Unit]
-Description=CLIMATECLOCK automatic startup service
+Description=ClimateClock
 
 [Service]
 Type=simple
 Restart=always
-ExecStart=/home/${FIRST_USER_NAME}/climateclock.py
+RestartSec=2s
+ExecStart=/home/${FIRST_USER_NAME}/clock/climateclock.py
+WorkingDirectory=/home/${FIRST_USER_NAME}/clock
 
 [Install]
 WantedBy=multi-user.target
@@ -30,22 +32,21 @@ cat > "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/install_rgbmatrix.sh" <<EOF
 cd "/home/${FIRST_USER_NAME}"
 [ -d rpi-rgb-led-matrix ] || git clone https://github.com/hzeller/rpi-rgb-led-matrix
 cd rpi-rgb-led-matrix
-git checkout e3dd56dcc0408862f39cccc47c1d9dea1b0fb2d2
-make build-python HARDWARE_DESC=adafruit-hat USER_DEFINES="-DDISABLE_HARDWARE_PULSES" PYTHON=/usr/bin/python3
-make install-python HARDWARE_DESC=adafruit-hat USER_DEFINES="-DDISABLE_HARDWARE_PULSES" PYTHON=/usr/bin/python3
+git checkout dfc27c15c224a92496034a39512a274744879e86
+make build-python HARDWARE_DESC=adafruit-hat-pwm PYTHON=/usr/bin/python3
+make install-python HARDWARE_DESC=adafruit-hat-pwm PYTHON=/usr/bin/python3
 EOF
 chmod +x "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/install_rgbmatrix.sh"
 
 
 # Build and install the python library from within the emulated target system
-# TODO: Make this whole process repeatable for those who update their system python version
+# TODO: Use some kind of package
 on_chroot << EOF
     echo "Adding rpi-rgb-led-matrix python library..."
     cd "/home/${FIRST_USER_NAME}"
 
-    # Pull the climate-clock-kit repo and modify the shebang for a virtualenv
     [ -d climate-clock-kit ] || git clone https://github.com/beautifultrouble/climate-clock-kit
-    ln -sf climate-clock-kit/clock/climateclock.py
+    ln -sf climate-clock-kit/clock
 
     # Build the matrix library
     ./install_rgbmatrix.sh
@@ -58,6 +59,17 @@ EOF
 # Replicate the steps for configuring the RTC from rgb-matrix.sh here:
 # https://github.com/adafruit/Raspberry-Pi-Installer-Scripts/blob/master/rgb-matrix.sh
 on_chroot << EOF
+    reconfig() {
+        grep $2 $1 >/dev/null
+        if [ $? -eq 0 ]; then
+            # Pattern found; replace in file
+            sed -i "s/$2/$3/g" $1 >/dev/null
+        else
+            # Not found; append (silently)
+            echo $3 | sudo tee -a $1 >/dev/null
+        fi
+    }
+
     echo "Configuring realtime clock..."
 
     # I believe the net effect of this command (on a fresh system) is to:
@@ -65,12 +77,15 @@ on_chroot << EOF
     #   * Add "dtparam=i2c_arm" to /boot/config.txt
     /usr/bin/raspi-config nonint do_i2c 0
 
-    # Do additional RTC setup for DS1307
-    printf "\n# For RGB Matrix HAT RTC\n" >> /boot/config.txt
-    printf "dtoverlay=i2c-rtc,ds1307\n" >> /boot/config.txt
-
     # Comment out line which causes hwclock-set to exit when systemd is running
     sed -i '/if \[ -e \/run\/systemd\/system \] ; then/,+2 s/^#*/#/' /lib/udev/hwclock-set
+
+    echo "Configuring additional hardware..."
+
+    # Do additional RTC setup for DS1307
+    reconfig /boot/config.txt "^.*dtoverlay=i2c-rtc.*$" "dtoverlay=i2c-rtc,ds1307"
+    # Disable sound ('easy way' -- kernel module not blacklisted)
+    reconfig /boot/config.txt "^.*dtparam=audio.*$" "dtparam=audio=off"
 EOF
 
 
